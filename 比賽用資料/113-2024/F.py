8811b8cc9f857b689863f4d6a71f492cee0fe674
Fatal-Error114514 <shenfeipang@gmail.com>
2024-12-21 18:04:37 +0800

    deleted:    dijkstra.py 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/A.py" 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/B.py" 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/C.py" 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/D.py" 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/E.py" 	new file:   "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/113-2024/F.py" 	renamed:    audiophobia.py -> "\346\257\224\350\263\275\347\224\250\350\263\207\346\226\231/test/audiophobia.py"

---
+++ 比賽用資料/113-2024/F.py
@@ -0,0 +1,7 @@
+m, n, n, p = map(int,input().split())
+A = []
+B = []
+for i in range(m):
+    A.append(list(map(int,input().split())))
+for i in range(n):
+    B.append(list(map(int,input().split())))
